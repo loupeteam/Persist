e95ed30a0795d33d246a606338599c2f2cc76ee0
ryan <ryan@loupe.team>
2023-10-05 15:13:57 -0700

    Add example AS project for using persist/persister

---
+++ example/AsProject/Logical/Libraries/Loupe/messagebox/MessageBox_Global.h
@@ -0,0 +1,18 @@
+/********************************************************************
+ * COPYRIGHT --  
+ ********************************************************************
+ * Library: MessageBox
+ * File: MessageBox_Global.h
+ * Author: Josh
+ * Created: Aug 1, 2013
+ *******************************************************************/
+
+#include <bur/plctypes.h>
+#include <string.h>
+
+MessageBox_typ * MessageBoxList[MESSAGEBOX_MAX+1];
+UDINT CurrentMessageID;
+
+unsigned short MessageBox_Get(unsigned short ID , unsigned long MessageBox );
+unsigned long MessageBox_AddMsg_full(unsigned short ID , unsigned long Message, unsigned long MessageIndex, unsigned long Buttons, BOOL Priority);
+unsigned long PromptBox_AddMsg_full(unsigned short ID , unsigned long Message, unsigned long MessageIndex,unsigned long UserString, unsigned long Buttons, BOOL Priority );
---
+++ example/AsProject/Logical/Libraries/Loupe/messagebox/VersionHistory.txt
@@ -0,0 +1,11 @@
+0.1.3 - Add 3, 4, and 5 button support
+0.1.2 - Add minimum time between showing message boxes
+0.1.1 - Migrate from AsString to AsBrStr.
+
+0.01.0 - Fixed typo in type. Promp->Prompt
+0.00.4 - 20150619 - Fixed issue that caused input text to 
+					show when it shouldn't if HMI first 
+					call started with a message
+0.00.3 - 20150424 - Added input text
+0.00.2 - 20150320 - Added active bit
+0.00.1 - 20131115 - Start versioning
